4cab30001c7505d2cd01a2dca0b6c23b0b9f0405
Hitarth <hitarth.sharma@outlook.com>
2017-03-07 13:15:58 +1300

    Tweaked state to be more correct when user is signed out

--- public/js/noteItDown.js
+++ public/js/noteItDown.js
@@ -1,231 +1,235 @@
 var NoteItDown = function(options){
 
-    let $noteContainerId = options.noteContainerId || 'note-it-down';
-    let $signInStatusLabelId = options.signInStatusLabelId || 'nid-sign-in-status';
-    let $signInButtonId = options.signInButtonId || 'nid-sign-in';
-    let $dataStatusLabelId = options.dataStatusLabelId || 'nid-data-status';
-    let $newNoteButtonId = options.newNoteButtonId || 'nid-new-note';
-    let $notesListId = options.notesListId || 'nid-notes-list';
-    let $noteItemLinkClass = options.noteItemLinkClass || 'nid-note-item-link';
-    let fireDb = firebase.database();
-    let uid = null;
+  let $noteContainerId = options.noteContainerId || 'note-it-down';
+  let $signInStatusLabelId = options.signInStatusLabelId || 'nid-sign-in-status';
+  let $signInButtonId = options.signInButtonId || 'nid-sign-in';
+  let $dataStatusLabelId = options.dataStatusLabelId || 'nid-data-status';
+  let $newNoteButtonId = options.newNoteButtonId || 'nid-new-note';
+  let $notesListId = options.notesListId || 'nid-notes-list';
+  let $noteItemLinkClass = options.noteItemLinkClass || 'nid-note-item-link';
+  let fireDb = firebase.database();
+  let uid = null;
 
-    /**
-     * Function called when clicking the Login/Logout button.
-     */
-    function toggleSignIn() {
-        if (!firebase.auth().currentUser) {
+  /**
+   * Function called when clicking the Login/Logout button.
+   */
+  function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
 
-            let provider = new firebase.auth.GoogleAuthProvider();
+      let provider = new firebase.auth.GoogleAuthProvider();
 
-            provider.addScope('https://www.googleapis.com/auth/plus.login');
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
 
-            firebase.auth().signInWithPopup(provider).then(function(result) {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                let token = result.credential.accessToken;
-                // The signed-in user info.
-                let user = result.user;
-            }).catch(function(error) {
-                // Handle Errors here.
-                let errorCode = error.code;
-                let errorMessage = error.message;
-                // The email of the user's account used.
-                let email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                let credential = error.credential;
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        let token = result.credential.accessToken;
+        // The signed-in user info.
+        let user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // The email of the user's account used.
+        let email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        let credential = error.credential;
 
-                if (errorCode === 'auth/account-exists-with-different-credential') {
-                    alert('You have already signed up with a different auth provider for that email.');
-                    // If you are using multiple auth providers on your app you should handle linking
-                    // the user's accounts here.
-                } else {
-                    console.error(error);
-                }
-
-            });
-
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('You have already signed up with a different auth provider for that email.');
+          // If you are using multiple auth providers on your app you should handle linking
+          // the user's accounts here.
         } else {
-            firebase.auth().signOut();
+          console.error(error);
         }
-        document.getElementById($signInButtonId).disabled = true;
 
+      });
+
+    } else {
+      firebase.auth().signOut();
     }
+    document.getElementById($signInButtonId).disabled = true;
 
-    /**
-     * Function for getting reference to note with given key
-     */
-    function getNoteRef(noteKey){
-        return fireDb.ref(`notes/${noteKey}`);
-    }
+  }
 
-    /**
-     * Function for updating current user's lastNote
-     */
-    function updateLastNote(noteKey, uid){
-        return fireDb.ref(`users/${uid}`).update({
-            lastNote: noteKey
-        });
-    }
+  /**
+   * Function for getting reference to note with given key
+   */
+  function getNoteRef(noteKey){
+    return fireDb.ref(`notes/${noteKey}`);
+  }
 
-    /**
-     * Function called for initializing a note at given ref for current user
-     */
-    function initNote(noteKey, uid){
-        let noteRef = getNoteRef(noteKey);
+  /**
+   * Function for updating current user's lastNote
+   */
+  function updateLastNote(noteKey, uid){
+    return fireDb.ref(`users/${uid}`).update({
+      lastNote: noteKey
+    });
+  }
 
-        // Create CodeMirror (with lineWrapping on) - after ensuring the container is empty.
-        let noteContainer = document.getElementById($noteContainerId);
-        noteContainer.innerHTML = "";
-        let codeMirror = CodeMirror(noteContainer, { lineWrapping: true });
+  /**
+   * Function called for initializing a note at given ref for current user
+   */
+  function initNote(noteKey, uid){
+    let noteRef = getNoteRef(noteKey);
 
-        // Create Firepad (with rich text toolbar and shortcuts enabled).
-        let note = Firepad.fromCodeMirror(noteRef, codeMirror,
-            { richTextShortcuts: true, richTextToolbar: true,
-                defaultText: 'Start noting things down...',
-                'userId' : uid });
+    // Create CodeMirror (with lineWrapping on) - after ensuring the container is empty.
+    let noteContainer = document.getElementById($noteContainerId);
+    noteContainer.innerHTML = "";
+    let codeMirror = CodeMirror(noteContainer, { lineWrapping: true });
 
-        //Remove Firepad logo
-        document.getElementsByClassName('powered-by-firepad')[0].outerHTML='';
+    // Create Firepad (with rich text toolbar and shortcuts enabled).
+    let note = Firepad.fromCodeMirror(noteRef, codeMirror,
+      { richTextShortcuts: true, richTextToolbar: true,
+        defaultText: 'Start noting things down...',
+        'userId' : uid });
 
-        note.on('ready', function() {
-            //Update status
-            document.getElementById($dataStatusLabelId).textContent = 'Data synced.';
+    //Remove Firepad logo
+    document.getElementsByClassName('powered-by-firepad')[0].outerHTML='';
 
-        });
+    note.on('ready', function() {
+      //Update status
+      document.getElementById($dataStatusLabelId).textContent = 'Data synced.';
 
-        let nidSynced = true;
+    });
 
-        note.on('synced', function(isSynced) {
-            // isSynced will be false immediately after the user edits the pad,
-            // and true when their edit has been saved to Firebase.
-            if(nidSynced != isSynced){
-                if(isSynced){
-                    document.getElementById($dataStatusLabelId).textContent = 'Data synced.';
-                }else{
-                    document.getElementById($dataStatusLabelId).textContent = 'Syncing...';
-                }
-                nidSynced = isSynced;
-            }
-        });
+    let nidSynced = true;
 
-        updateLastNote(noteKey, uid);
-    }
-
-    /**
-     * Function called for creating and initializing a new note
-     */
-    function createNewNote(){
-        //Create a new Firepad at /notes/$noteId
-        let noteRef = fireDb.ref('notes').push();
-
-        updateLastNote(noteRef.key, uid).then(() => {
-            fireDb.ref(`users/${uid}/notes/${noteRef.key}`).set(true);
-        });
-
-        //Initialize it
-        initNote(noteRef.key, uid);
-
-        return noteRef;
-    }
-
-    /**
-     * Function for fetching and rendering list of user's notes
-     */
-    function initNotesList(uid){
-        if(uid){
-            let userPostsRef = fireDb.ref(`users/${uid}/notes`);
-            let notesList = document.getElementById($notesListId);
-            notesList.innerHTML = '';
-            userPostsRef.on('child_added', (data) => {
-                let noteItem = document.createElement('li');
-                let noteLink = document.createElement('a');
-                noteItem.className += 'nid-note-item';
-                noteLink.className += 'nid-note-item-link';
-                noteLink.href = '#';
-                noteLink.innerText = data.key;
-                noteItem.appendChild(noteLink);
-                noteItem.addEventListener('click', (e) => {
-                    return initNote(e.srcElement.innerText, uid);
-                }, false);
-                notesList.appendChild(noteItem);
-            });
+    note.on('synced', function(isSynced) {
+      // isSynced will be false immediately after the user edits the pad,
+      // and true when their edit has been saved to Firebase.
+      if(nidSynced != isSynced){
+        if(isSynced){
+          document.getElementById($dataStatusLabelId).textContent = 'Data synced.';
+        }else{
+          document.getElementById($dataStatusLabelId).textContent = 'Syncing...';
         }
+        nidSynced = isSynced;
+      }
+    });
+
+    updateLastNote(noteKey, uid);
+  }
+
+  /**
+   * Function called for creating and initializing a new note
+   */
+  function createNewNote(){
+    //Create a new Firepad at /notes/$noteId
+    let noteRef = fireDb.ref('notes').push();
+
+    updateLastNote(noteRef.key, uid).then(() => {
+      fireDb.ref(`users/${uid}/notes/${noteRef.key}`).set(true);
+    });
+
+    //Initialize it
+    initNote(noteRef.key, uid);
+
+    return noteRef;
+  }
+
+  /**
+   * Function for fetching and rendering list of user's notes
+   */
+  function initNotesList(uid){
+    if(uid){
+      let userPostsRef = fireDb.ref(`users/${uid}/notes`);
+      let notesList = document.getElementById($notesListId);
+      notesList.innerHTML = '';
+      userPostsRef.on('child_added', (data) => {
+        let noteItem = document.createElement('li');
+        let noteLink = document.createElement('a');
+        noteItem.className += 'nid-note-item';
+        noteLink.className += 'nid-note-item-link';
+        noteLink.href = '#';
+        noteLink.innerText = data.key;
+        noteItem.appendChild(noteLink);
+        noteItem.addEventListener('click', (e) => {
+          return initNote(e.srcElement.innerText, uid);
+        }, false);
+        notesList.appendChild(noteItem);
+      });
     }
+  }
 
-    /**
-     * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-     *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-     *    out, and that is where we update the UI.
-     */
-    function initApp() {
-        // Listening for auth state changes.
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                // User is signed in.
-                let displayName = user.displayName;
-                let email = user.email;
-                let emailVerified = user.emailVerified;
-                let photoURL = user.photoURL;
-                let isAnonymous = user.isAnonymous;
-                let providerData = user.providerData;
-                uid = user.uid;
+  /**
+   * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+   *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+   *    out, and that is where we update the UI.
+   */
+  function initApp() {
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        let displayName = user.displayName;
+        let email = user.email;
+        let emailVerified = user.emailVerified;
+        let photoURL = user.photoURL;
+        let isAnonymous = user.isAnonymous;
+        let providerData = user.providerData;
+        uid = user.uid;
 
-                document.getElementById($signInStatusLabelId).textContent = `Signed in as ${displayName}`;
-                document.getElementById($signInButtonId).value = 'Sign out';
+        document.getElementById($signInStatusLabelId).textContent = `Signed in as ${displayName}`;
+        document.getElementById($signInButtonId).value = 'Sign out';
 
-                document.getElementById($dataStatusLabelId).textContent = 'Syncing your data now...';
+        document.getElementById($dataStatusLabelId).textContent = 'Syncing your data now...';
 
-                let noteRef = null;
+        let noteRef = null;
 
-                //Find uid in users
-                let nidUser = null;
-                fireDb.ref(`users/${uid}`).once('value').then(snapshot => {
-                    nidUser = snapshot.val();
+        //Find uid in users
+        let nidUser = null;
+        fireDb.ref(`users/${uid}`).once('value').then(snapshot => {
+          nidUser = snapshot.val();
 
-                    //If user is found
-                    if (nidUser) {
-                        //Retrieve lastNote or note that was created most recently
-                        if (nidUser.lastNote) {
-                            noteRef = getNoteRef(nidUser.lastNote);
-                        } else if (nidUser.notes && nidUser.notes.length > 0) {	// TODO Fix this as notes is not an array
-                            noteRef = getNoteRef(nidUser.notes.pop());
-                        } else {
-                            //Create a new Firepad at /notes/$noteId
-                            noteRef = fireDb.ref('notes').push();
-                        }
-                    } else {
-                        noteRef = createNewNote(fireDb, uid);
-
-                        //Add user to the users list
-                        fireDb.ref(`users/${uid}`).set({
-                            lastNote: noteRef.key
-                        }).then(() => {
-                            fireDb.ref(`users/${uid}/notes/${noteRef.key}`).set(true);
-                        });
-                    }
-
-                    initNote(noteRef.key, uid);
-
-                    initNotesList(uid);
-                });
-
+          //If user is found
+          if (nidUser) {
+            //Retrieve lastNote or note that was created most recently
+            if (nidUser.lastNote) {
+              noteRef = getNoteRef(nidUser.lastNote);
+            } else if (nidUser.notes && nidUser.notes.length > 0) {	// TODO Fix this as notes is not an array
+              noteRef = getNoteRef(nidUser.notes.pop());
             } else {
-                // User is signed out.
-                document.getElementById($signInStatusLabelId).textContent = 'Signed out';
-                document.getElementById($signInButtonId).value = 'Sign in with Google';
-                document.getElementById($noteContainerId).innerHTML = '';
-                document.getElementById($notesListId).innerHTML = '';
-                uid = null;
+              //Create a new Firepad at /notes/$noteId
+              noteRef = fireDb.ref('notes').push();
             }
-            document.getElementById($signInButtonId).disabled = false;
+          } else {
+            noteRef = createNewNote(fireDb, uid);
 
+            //Add user to the users list
+            fireDb.ref(`users/${uid}`).set({
+              lastNote: noteRef.key
+            }).then(() => {
+              fireDb.ref(`users/${uid}/notes/${noteRef.key}`).set(true);
+            });
+          }
+
+          initNote(noteRef.key, uid);
+
+          initNotesList(uid);
+
+          document.getElementById($newNoteButtonId).disabled = false;
         });
 
-        document.getElementById($signInButtonId).addEventListener('click', toggleSignIn, false);
-        document.getElementById($newNoteButtonId).addEventListener('click', createNewNote, false);
-    }
+      } else {
+        // User is signed out.
+        document.getElementById($signInStatusLabelId).textContent = 'Signed out';
+        document.getElementById($signInButtonId).value = 'Sign in with Google';
+        document.getElementById($noteContainerId).innerHTML = '';
+        document.getElementById($notesListId).innerHTML = '';
+        document.getElementById($dataStatusLabelId).innerHTML = '';
+        document.getElementById($newNoteButtonId).disabled = true;
+        uid = null;
+      }
+      document.getElementById($signInButtonId).disabled = false;
 
-    return {
-        init: initApp
-    };
+    });
+
+    document.getElementById($signInButtonId).addEventListener('click', toggleSignIn, false);
+    document.getElementById($newNoteButtonId).addEventListener('click', createNewNote, false);
+  }
+
+  return {
+    init: initApp
+  };
 };
